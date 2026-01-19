5968fd54fea0039fb9e979fa01968526da36afdc
Federico Chocobar <chocoeniac@gmail.com>
2026-01-19 19:23:50 -0300

    Initial commit: Nest.js project structure

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,21 @@
+﻿import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World! TP Academy NestJS Project"', () => {
+      expect(appController.getHello()).toBe('Hello World! TP Academy NestJS Project');
+    });
+  });
+});
---
+++ src/app.controller.ts
@@ -0,0 +1,9 @@
+﻿import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Hello World! TP Academy NestJS Project';
+  }
+}
